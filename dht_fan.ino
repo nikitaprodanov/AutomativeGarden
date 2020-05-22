06ccf1a67b81a053142967a725b0a211c00d8839
Nikita <uchihanaruto9371@gmail.com>
2020-05-22 21:59:37 +0300

    Printing values for testing purposes

--- dht_fan.ino
+++ dht_fan.ino
@@ -8,11 +8,18 @@ dht11 DHT11;
 
 void setup(){
   pinMode(FAN, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop()
 {
   DHT11.read(DHT_PIN);
+
+  Serial.print("Humidity (%): ");
+  Serial.println((float)DHT11.humidity, 2);
+
+  Serial.print("Temperature (C): ");
+  Serial.println((float)DHT11.temperature, 2);
 
   if(DHT11.temperature < tooHot) {
     digitalWrite(FAN, HIGH);
