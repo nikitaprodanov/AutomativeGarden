e01e701602aef521e9adaa0c442bbe063025e73a
Nikita <uchihanaruto9371@gmail.com>
2020-05-31 20:33:50 +0300

    Timer for day and night routine

--- final.ino
+++ final.ino
@@ -4,14 +4,21 @@ dht11 DHT11; //Initializing the dht sensor
 
 //Constants:
 
-#define LED 9 //Pin to control the led strip
-#define DHT_PIN 7 //Pin to read the value of the dht sensor 
-#define FAN 3 //Pin that controls the dc fan
+#define LED 5 //Pin to control the led strip
+#define DHT_PIN 12 //Pin to read the value of the dht sensor 
+#define FAN 9 //Pin that controls the dc fan
 #define tooHot 26 //Constant temperature value
-#define waterPump 5; //Pin that controls the water pump
+#define waterPump 3; //Pin that controls the water pump
 const int hygrometer = A0; //Pin that reads the output of the soil humidity sensor
 
+//Variables:
+
 int humidity_value; //Variable that saves the value that was read by the soil humidity sensor
+bool isDay = 1; //This bool tells if it is daytime
+bool isNight = 0; //This bool tells if it is nighttime
+int timer = 0; //This variable imitates the behaviour of a timer
+#define day_hours 32400;
+#define night_hours 54000;
 
 //Setting up the pins
 void setup(){
@@ -23,8 +30,6 @@ void setup(){
 //Main loop
 void loop()
 {
-  digitalWrite(LED, HIGH); //Turning on the led strip
-
   DHT11.read(DHT_PIN); //Reading from the dht sensor
 
   //Checking if the temperature value from the sensor is higher than 26 C
@@ -48,5 +53,29 @@ void loop()
       digitalWrite(waterPump,LOW); //Else turn the water pump off
   }
 
-  delay(150); //Small delay before the next iteration
+  delay(2000); //Small delay before the next iteration
+
+  //Led strip day and night routine
+  if (isDay && timer < day_hours) { //If it is day and the strip hasn't been on for 9hours
+    digitalWrite(LED, HIGH) //Led strip is on
+    timer += 2; //Timer increases with 2 (representing seconds)
+    isNight = 0; //It is not night
+  }
+  else if (isDay && timer >= day_hours) { //If it is day and the strip has been working more than 9hours
+    digitalWrite(LED, LOW); //Turning off the led strip
+    isNight = 1; //It is now night
+    isDay = 0; //It's no longer day
+    timer = 0; //Reset the timer to start counting the 15 hours of no light
+  }
+  else if (isNight && timer < night_hours) { //If it is night and the strip has't been working for less than 15 hours
+    digitalWrite(LED, LOW); //Strip is off
+    timer += 2; //Timer increases with 2 (representing seconds)
+    isDay = 0; //It's not day
+  }
+  else if (isNight && timer >= night_hours) { //If it is night and the strip has not been working for more than 15 hours
+    digitalWrite(LED, HIGH); //Led strip should turn on
+    timer = 0; //Reset the timer to count the 9 hours of day
+    isDay = 1; //It is now day
+    isNight = 0; //It is no longer night
+  }
 }
